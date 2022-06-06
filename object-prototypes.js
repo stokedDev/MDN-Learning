3ee81126f90fa31feaa37b05bd86de08a563a824
mauriceRogers <scsurfermaurice@gmail.com>
2022-06-06 00:58:40 -0700

    Finished working through Object Prototype Guide

--- object-prototypes.js
+++ object-prototypes.js
@@ -45,6 +45,42 @@ mauro.chill('watching a show on netflix') // I'm chilling, watching a show on ne
 
 // mauro besides it's prototype personPrototype, is an empty object.
 
-/* Is the prototype where the built in properties like toString are located? Yes. */
+/* Is a prototype where the built in properties like toString are located? Yes, until the end of the prototype chain which is always null. */
+console.log({name: 'J', pet: 'Cutie'}) // When this object is opened, it has 3 properties, (name, pet, [[prototype]])
 /* __proto__ is the standard property for looking inside of an object's prototype where built-in properties can be found. */
-console.log({}.__proto__) // takes us straight to the built-in properties on the prototype of this object
+console.log({}.__proto__.constructor) /* takes us straight to the built-in properties on the prototype of this object. Without accessing __proto__ [[prototype]] is it's only property */
+
+/* Using a constructor */
+
+const charPrototype = {
+    greet() {
+      return `Hello, my name is ${this.name}!`;
+    }
+  }
+  
+  function Person(name) { //constructor function
+    this.name = name;
+  }
+  const jerry = new Person('Jerry')
+  jerry.name // returns Jerry, as the jerry object was created 
+  jerry.__proto__ // returns Person {}
+  Person.prototype = charPrototype;
+  Person.prototype.constructor = Person; /* setting the constructor of Person.prototype (which is now charPrototype) */
+  /* Why not just say Person.greet = function(){return `Hello, I'm ${this.name}!} so
+  we can access the greet method from the constructor? */
+  Person.greet = function(){return `Hello, I'm ${this.name}!`} /* methods shouldn't be placed on constructors but instead on the prototype */
+const dan = new Person('Dan')
+dan.greet()// Hello, my name is Dan! 
+/* When dan.greet() is called, what happens is the greet method is found in dan's prototype and at the same time the name property is accessed through dan's prototype's constructor function which is Person {}. An argument passed in the place of a parameter is 'Dan'. 
+ */
+
+/* Own Properties */
+
+Object.hasOwn(dan, 'greet') /* false, because the greet method is not located directly on the surface level of the dan obj but instead on the prototype */
+
+Object.hasOwn(dan, 'name') // true, because the constructor function Person
+
+/* Prototypes and inheritance */
+
+// Objects of Professors and Students can share common methods like greet() on their prototype and data properties (like name) on their constructor. As it's a common pattern to define methods on the prototype as they stay the same, while data properties are defined on the constructor to feed values to the methods on the prototype.
+  
