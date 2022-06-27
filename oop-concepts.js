1431ad9467230738d0f009454561953242b77bde
mauriceRogers <scsurfermaurice@gmail.com>
2022-06-26 19:35:06 -0700

    + script el & encapsulation info with JS
    Added script element to index.html and elaborated on encapsulation with JS and comments

--- oop-concepts.js
+++ oop-concepts.js
@@ -29,8 +29,29 @@ Unlike procedural programming where functions and variables are created without 
 OOP classes make it so we can keep related member methods and member variables together in one place.
 Keywords can be used when creating definitions within a class to make such definitions public or private.
 If within a class a definition is made public, it is accessible from outside.
-If within a class a definition is made private, it is not accessible from outside.
+If within a class a definition is made private, it is not accessible from outside but can be from the inside.
 
+Heres JS code to give an example of encapsulation, # is a keyword to make a class field private:
+*/
+class Persont{ /* I put a "t" at the end because the there is a conflicting class name in another file in this directory. */
+#gender; /* We need to declare this private class data field here to make it usable yet not accessible from the outside if it's used in a method that we call but it can't be accessed directly from the outside of the class 
+as in mally.#gender  */
+    constructor(name,gender,mood,location){
+        this.#gender = gender; // private class field that can't be accessed directly from the outside
+        this.mood = mood;
+        this.name = name;
+        this.location = location;
+    }
+    #dance(){ /* this class method is not directly accessible from the outside of this class but is accessible in other methods of this class such as dancingWhere() for example. */
+        return `${this.name} is ${this.mood} and is dancing in a ${this.#gender} way.`
+    }
+    dancingWhere(){ // even when called from the outside of the class, this method will return a string which includes the called private method #dance(), but #dance() can't be called from the outside, but in dancingWhere() it is called privately inside the Persont class.
+        return `${this.location} ${this.#dance()}`
+    }
+}
+const mally = new Persont('Mally', 'female', 'happy', 'In a grass field,');
+mally.dancingWhere() // returns In a grass field, Mally is happy and is dancing in a female way.
+/*
 OOP and JavaScript:
 Constructors and the prototype chain allow for similiar control as "classical" OOP.
 JS is capable of being used as classbased OOP which is discussed in the next MDN article.
