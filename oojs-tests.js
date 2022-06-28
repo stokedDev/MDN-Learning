1cdfa5da398ba73cb836f82672a62e83eedd85e3
mauriceRogers <scsurfermaurice@gmail.com>
2022-06-28 14:22:32 -0700

    completed OOJS test 2

--- oojs-tests.js
+++ oojs-tests.js
@@ -3,7 +3,6 @@
 
 /* OOJS test 1 */
 class Shape {
-
     name;
     sides;
     sideLength;
@@ -13,12 +12,25 @@ class Shape {
           this.sideLength = sideLength;
   }
   calcPerimeter(){
-      return (this.sides * this.sideLength);
+      return this.sides * this.sideLength;
   }
 }
 
 const square = new Shape('square', 4, 5);
 square.calcPerimeter() // 20
-
 const triangle = new Shape('triangle', 3, 3);
 triangle.calcPerimeter() // 9
+
+/* OOJS test 2 */
+
+class Square extends Shape {
+constructor(sideLength){
+    super('square',4, sideLength);
+}
+calcArea(){
+    return this.sideLength**2
+}
+}
+const square2 = new Square(6)
+square2.calcArea() // 36
+square2.calcPerimeter()// 24
