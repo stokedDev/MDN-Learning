f2c8d7207bd2c35656737c6e8f24e3ac9e957e8a
mauriceRogers <scsurfermaurice@gmail.com>
2022-08-07 21:51:40 -0700

    Use Promise.all()
    Async js using .then(),.catch(), and .finally(). Process a combination of multiple promises.

--- asynchronous-js/how-to-use-promises.js
+++ asynchronous-js/how-to-use-promises.js
@@ -1,11 +1,24 @@
-const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
-// here we are sending a request to the url passed to the fetch promise
-fetchPromise.then(response => {/* This first handler receives the response*/
-    if(!response.ok){/* if we don't get a 200 (OK) status code we throw an error */
-        throw new Error(`HTTP error: ${response.status}`)
-    }
-    return response.json();/* the .json() method converts the JSON to a JS object */
-}).then(data => { /* This handler accesses the JS object returned by the last handler */
-    console.log(data[0].name); /* Here we log the first object and it's name property which is "baked beans" */
+// const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+// // here we are sending a request to the url passed to the fetch promise
+// fetchPromise.then(response => {/* This first handler receives the response*/
+//     if(!response.ok){/* if we don't get a 200 (OK) status code we throw an error */
+//         throw new Error(`HTTP error: ${response.status}`)
+//     }
+//     return response.json();/* the .json() method converts the JSON to a JS object */
+// }).then(data => { /* This handler accesses the JS object returned by the last handler */
+//     console.log(data[0].name); /* Here we log the first object and it's name property which is "baked beans" */
+// })
+// .catch(error => console.error(`Couldn't get products: ${error}`)) /* This handler deals with errors if they come up */
+
+// /* Combining Multiple Promises */
+
+const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+
+Promise.all([fetchPromise1, fetchPromise2])
+.then(responses => {
+ return responses.map(el => el.json());
 })
-.catch(error => console.error(`Couldn't get products: ${error}`)) /* This handler deals with errors if they come up */
+.then(data => console.log([...data]))
+.catch(error => console.error(`Here's the error: ${error}`))
+.finally(setTimeout(() => console.log('This runs no matter what.'),500))
