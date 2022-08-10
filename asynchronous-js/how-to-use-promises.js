11a3cb5739a4555adb492c674bff721845a9fa08
mauriceRogers <scsurfermaurice@gmail.com>
2022-08-09 22:50:54 -0700

    Handle 2 promises using async await syntax

--- asynchronous-js/how-to-use-promises.js
+++ asynchronous-js/how-to-use-promises.js
@@ -24,11 +24,49 @@
 // .catch(error => console.error(`Here's the error: ${error}`))
 // .finally(setTimeout(() => console.log('This runs no matter what.'),500))
 
-const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
-const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
-Promise.any([fetchPromise1, fetchPromise2]) /* Promise.any() will return a fulfilled promise if any of the promises passed to it are fulfilled. It returns a rejected promise only if all the promises in it's passed array are rejected. */
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(`Here's the error: ${error}`))
-.finally(setTimeout(() => 
-console.log('I am logged when the promise is settled, whether fulfilled or rejected.'),500)); /* The .finally() handler is called when the promise is settled, whether fulfilled or rejected. */
+// const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+// const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+// Promise.any([fetchPromise1, fetchPromise2]) /* Promise.any() will return a fulfilled promise if any of the promises passed to it are fulfilled. It returns a rejected promise only if all the promises in it's passed array are rejected. */
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error(`Here's the error: ${error}`))
+// .finally(setTimeout(() => 
+// console.log('I am logged when the promise is settled, whether fulfilled or rejected.'),500)); /* The .finally() handler is called when the promise is settled, whether fulfilled or rejected. */
+
+async function fetchProducts(){
+    try{
+        const response = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+        if(!response.ok){
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+        const data = await response.json()
+        return data;
+    }
+    catch(error){
+        console.error(`Could not get products because of the following error: ${error}`)
+    }
+}
+const promise = fetchProducts()
+promise.then(data => console.log(data));
+
+
+async function fetchSuperheroes(){
+    try{
+        const response = await fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+        if(!response.ok) throw new Error(`HTTP error: ${response.status}`)
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        console.error(`Couldn't fetch superheroes because of the following error: ${error}`)
+    }
+}
+const promise2 = fetchSuperheroes()
+promise2.then(data => console.log(data))
+
+
+
+
+
+
+
