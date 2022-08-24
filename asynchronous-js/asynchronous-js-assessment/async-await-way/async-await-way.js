c667561e2cb41e6e1c81460ec10c07cc19e94485
mauriceRogers <scsurfermaurice@gmail.com>
2022-08-23 23:03:28 -0700

    Animate 3 svg images one after another using async await

---
+++ asynchronous-js/asynchronous-js-assessment/async-await-way/async-await-way.js
@@ -0,0 +1,29 @@
+const aliceTumbling = [
+    { transform: 'rotate(0) scale(1)' },
+    { transform: 'rotate(360deg) scale(0)' }
+  ];
+  
+  const aliceTiming = {
+    duration: 2000,
+    iterations: 1,
+    fill: 'forwards'
+  }
+  
+  const alice1 = document.querySelector("#alice1");
+  const alice2 = document.querySelector("#alice2");
+  const alice3 = document.querySelector("#alice3");
+  
+  async function animations(){
+      try{
+        await alice1.animate(aliceTumbling, aliceTiming).finished;
+        await alice2.animate(aliceTumbling, aliceTiming).finished;
+        await alice3.animate(aliceTumbling, aliceTiming).finished;
+      }
+      catch(error){
+          console.log(error);
+      }
+      finally{
+        console.log('Animations promises settled.')
+      }
+  }
+  animations()
